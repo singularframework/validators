573897e0233810c6c411d2e4bb11f842dda2f060
Ramtin Soltani <ramtinsoltani93@gmail.com>
2020-08-26 10:31:43 -0700

    Added all validators (not tested) with definitions.

---
+++ src/definitions.ts
@@ -0,0 +1,188 @@
+import { ValidatorFunction, AsyncValidatorFunction, resolveRef, ValidationDefinition } from '@singular/core';
+import { DateTime, IANAZone } from 'luxon';
+
+const validators = {
+  string: <ValidatorFunction>(value => typeof value === 'string'),
+  boolean: <ValidatorFunction>(value => typeof value === 'boolean'),
+  number: <ValidatorFunction>(value => typeof value === 'number'),
+  object: <ValidatorFunction>(value => value && typeof value === 'object' && value.constructor === Object),
+  array: <ValidatorFunction>(value => value && typeof value === 'object' && value.constructor === Array),
+  not: (validator: ValidatorFunction|AsyncValidatorFunction) => {
+
+    return <AsyncValidatorFunction>(async (value, rawValues) => {
+
+      const result = await validator(value, rawValues);
+
+      if ( result instanceof Error ) return true;
+
+      return ! result;
+
+    });
+
+  },
+  length: (validator: ValidatorFunction|AsyncValidatorFunction) => {
+
+    return <AsyncValidatorFunction>((value, rawValues) => {
+
+      return validator(value.length, rawValues);
+
+    });
+
+  },
+  undefined: <ValidatorFunction>(value => value === undefined),
+  null: <ValidatorFunction>(value => value === null),
+  NaN: <ValidatorFunction>(value => isNaN(value)),
+  true: <ValidatorFunction>(value => value === true),
+  false: <ValidatorFunction>(value => value === false),
+  truthy: <ValidatorFunction>(value => !! value),
+  falsey: <ValidatorFunction>(value => ! value),
+  equal: (target: any) => <ValidatorFunction>(value => value === target),
+  equalRef: (ref: string) => <ValidatorFunction>((value, rawValues) => value === resolveRef(ref, rawValues)),
+  gt: (target: number) => <ValidatorFunction>(value => +value > target),
+  gtRef: (ref: string) => <ValidatorFunction>((value, rawValues) => +value > +resolveRef(ref, rawValues)),
+  gte: (target: number) => <ValidatorFunction>(value => +value >= target),
+  gteRef: (ref: string) => <ValidatorFunction>((value, rawValues) => +value >= +resolveRef(ref, rawValues)),
+  lt: (target: number) => <ValidatorFunction>(value => +value < target),
+  ltRef: (ref: string) => <ValidatorFunction>((value, rawValues) => +value < +resolveRef(ref, rawValues)),
+  lte: (target: number) => <ValidatorFunction>(value => +value <= target),
+  lteRef: (ref: string) => <ValidatorFunction>((value, rawValues) => +value <= +resolveRef(ref, rawValues)),
+  between: (min: number, max: number) => <ValidatorFunction>(value => +value >= min && +value <= max),
+  betweenEx: (min: number, max: number) => <ValidatorFunction>(value => +value > min && +value < max),
+  negative: <ValidatorFunction>(value => +value < 0),
+  positive: <ValidatorFunction>(value => +value > 0),
+  even: <ValidatorFunction>(value => +value % 2 === 0),
+  odd: <ValidatorFunction>(value => +value % 2 === 1),
+  exist: <ValidatorFunction>(value => value !== undefined),
+  match: (regex: RegExp) => <ValidatorFunction>(value => value.match && !! value.match(regex)),
+  include: (target: any) => <ValidatorFunction>(value => value.includes && value.includes(target)),
+  includeRef: (ref: string) => <ValidatorFunction>((value, rawValues) => value.includes && value.includes(resolveRef(ref, rawValues))),
+  includeAll: (...targets: any[]) => <ValidatorFunction>(value => {
+
+    if ( ! value.includes ) return false;
+
+    for ( const target of targets ) {
+
+      if ( ! value.includes(target) ) return false;
+
+    }
+
+    return true;
+
+  }),
+  in: (target: any) => <ValidatorFunction>(value => target.includes && target.includes(value)),
+  inRef: (ref: string) => <ValidatorFunction>((value, rawValues) => {
+
+    const target = resolveRef(ref, rawValues);
+
+    if ( ! target || ! target.includes ) return false;
+
+    return target.includes(value);
+
+  }),
+  enum: (enumerator: any) => <ValidatorFunction>(value => Object.values(enumerator).includes(value)),
+  date: <ValidatorFunction>(value => DateTime.fromJSDate(new Date(value)).isValid),
+  timezone: <ValidatorFunction>(value => value && IANAZone.isValidZone(value)),
+  or: (...validators: Array<ValidatorFunction|AsyncValidatorFunction>) => {
+
+    return async (value: any, rawValues?: any) => {
+
+      for ( const validator of validators ) {
+
+        const validatorResult = await validator(value, rawValues);
+
+        if ( validatorResult === true ) return true;
+
+      }
+
+      return false;
+
+    };
+
+  },
+  empty: <ValidatorFunction>(value => {
+
+    if ( typeof value === 'string' ) return value.length === 0;
+    if ( typeof value === 'number' ) return value === 0;
+    if ( value && typeof value === 'object' && value.constructor === Object ) return Object.keys(value).length === 0;
+    if ( value && typeof value === 'object' && value.constructor === Array ) return value.length === 0;
+
+    return false;
+
+  }),
+  zero: <ValidatorFunction>(value => +value === 0),
+  email: <ValidatorFunction>(value => typeof value === 'string' && !! value.match(/^[a-z0-9.\-_]+@[a-z0-9-]+\.[a-z]+$/i)),
+  children: (validator: ValidationDefinition|ValidatorFunction|AsyncValidatorFunction, localRefs?: boolean) => {
+
+    return async (value: any, rawValues?: any) => {
+
+      // If value is of type object
+      if ( value && typeof value === 'object' ) {
+
+        // If object
+        if ( value.constructor === Object ) {
+
+          // If validator is a direct function
+          if ( typeof validator === 'function' ) return await validator(value, localRefs ? value : rawValues);
+
+          // Otherwise (it's a validation definition object)
+          for ( const key in validator ) {
+
+            let keyValidator = validator[key];
+
+            // If key validator is ExecutableValidators, execute it to get a single validator function
+            if ( typeof keyValidator !== 'function' ) keyValidator = keyValidator.__exec();
+
+            const keyValidationResult = await keyValidator(value[key], localRefs ? value : rawValues);
+
+            // If validation didn't pass
+            if ( keyValidationResult instanceof Error || ! keyValidationResult ) return keyValidationResult;
+
+          }
+
+          // If all keys passed validation
+          return true;
+
+        }
+        // If array
+        else if ( value.constructor === Array ) {
+
+          // Iterate through the array
+          for ( const item of value ) {
+
+            // If validator is a direct function
+            if ( typeof validator === 'function' ) return await validator(item, localRefs ? item : rawValues);
+
+            // Otherwise (it's a validation definition object)
+            if ( ! item || typeof item !== 'object' || item.constructor !== Object ) return false;
+
+            for ( const key in validator ) {
+
+              let keyValidator = validator[key];
+
+              // If key validator is ExecutableValidators, execute it to get a single validator function
+              if ( typeof keyValidator !== 'function' ) keyValidator = keyValidator.__exec();
+
+              const keyValidationResult = await keyValidator(item[key], localRefs ? item : rawValues);
+
+              // If validation didn't pass
+              if ( keyValidationResult instanceof Error || ! keyValidationResult ) return keyValidationResult;
+
+            }
+
+          }
+
+          // If all keys passed validation
+          return true;
+
+        }
+
+      }
+
+      return false;
+
+    };
+
+  }
+};
+
+export default validators;
---
+++ src/validators.ts
@@ -0,0 +1,359 @@
+import { ValidatorFunction, AsyncValidatorFunction, resolveRef, ValidationDefinition } from '@singular/core';
+import validators from './definitions';
+
+export class Validators {
+
+  constructor(
+    private __validators: Array<ValidatorFunction|AsyncValidatorFunction> = [],
+    private __lengthAsValue: boolean = false,
+    private __negate: boolean = false,
+    private __optional: boolean = false,
+    private __refAsValue: string = null,
+    private __conditions: Array<ValidatorFunction|AsyncValidatorFunction> = []
+  ) { }
+
+  /** Wraps a validator if necessary based on this.__negate and this.__lengthAsValue. */
+  private __wrapIfNecessary(validator: ValidatorFunction|AsyncValidatorFunction): ValidatorFunction|AsyncValidatorFunction {
+
+    let wrapped = validator;
+
+    if ( this.__lengthAsValue ) wrapped = validators.length(wrapped);
+    if ( this.__negate ) wrapped = validators.not(wrapped);
+
+    return wrapped;
+
+  }
+
+  /** Returns a new Validators object with the given validator added to the array (also carries this.__lengthAsValue). */
+  private __addValidator(validator: ValidatorFunction|AsyncValidatorFunction) {
+
+    return new Validators(
+      this.__validators.concat(this.__wrapIfNecessary(validator)),
+      this.__lengthAsValue,
+      this.__negate,
+      this.__optional,
+      this.__refAsValue,
+      this.__conditions
+    );
+
+  }
+
+  /**
+  * NO NEED TO CALL THIS METHOD
+  *
+  * Finalizes the validators as one validator function (ANDs all used validators).
+  */
+  public __exec(): AsyncValidatorFunction {
+
+    return async (value: any, rawValues?: any) => {
+
+      // If value needs to be resolved from ref
+      if ( this.__refAsValue ) value = resolveRef(this.__refAsValue, rawValues);
+      // If whole validation is optional
+      if ( this.__optional && value === undefined ) return true;
+
+      // If conditional validation
+      if ( this.__conditions.length ) {
+
+        // AND all conditions
+        for ( const validator of this.__conditions ) {
+
+          const validatorResult = await validator(value, rawValues);
+
+          // Ignore when conditions are not true
+          if ( validatorResult instanceof Error || validatorResult === false ) return true;
+
+        }
+
+      }
+
+      // AND all validators
+      for ( const validator of this.__validators ) {
+
+        const validatorResult = await validator(value, rawValues);
+
+        if ( validatorResult instanceof Error || validatorResult === false ) return validatorResult;
+
+      }
+
+      return true;
+
+    };
+
+  }
+
+  // Aesthetics
+
+  /** Aesthetic property. */
+  public get should() { return this; }
+  /** Aesthetic property. */
+  public get be() { return this; }
+  /** Aesthetic property. */
+  public get and() { return this; }
+  /** Aesthetic property. */
+  public get have() { return this; }
+  /** Aesthetic property. */
+  public get a() { return this; }
+  /** Aesthetic property. */
+  public get an() { return this; }
+  /** Aesthetic property. */
+  public get been() { return this; }
+  /** Aesthetic property. */
+  public get has() { return this; }
+  /** Aesthetic property. */
+  public get of() { return this; }
+  /** Aesthetic property. */
+  public get to() { return this; }
+  /** Aesthetic property. */
+  public get belong() { return this; }
+  /** Aesthetic property. */
+  public get belongs() { return this; }
+  /** Aesthetic property. */
+  public get it() { return this; }
+  /** Aesthetic property. */
+  public get its() { return this; }
+  /** Aesthetic property. */
+  public get must() { return this; }
+  /** Aesthetic property. */
+  public get these() { return this; }
+  /** Aesthetic property. */
+  public get that() { return this; }
+  /** Aesthetic property. */
+  public get as() { return this; }
+  /** Aesthetic property. */
+  public get are() { return this; }
+  /** Aesthetic property. */
+  public get is() { return this; }
+  /** Aesthetic property. */
+  public get which() { return this; }
+  /** Aesthetic property. */
+  public get where() { return this; }
+  /** Aesthetic property. */
+  public get does() { return this; }
+
+  // Type checkers
+
+  /** Checks the value to be a string. */
+  public get string() { return this.__addValidator(validators.string); }
+  /** Checks the value to be a boolean. */
+  public get boolean() { return this.__addValidator(validators.boolean); }
+  /** Checks the value to be a number. */
+  public get number() { return this.__addValidator(validators.number); }
+  /** Checks the value to be an object. */
+  public get object() { return this.__addValidator(validators.object); }
+  /** Checks the value to be an array. */
+  public get array() { return this.__addValidator(validators.array); }
+
+  // Mutators
+
+  /** Negates all validators from this point forward. */
+  public get not() {
+
+    return new Validators(
+      this.__validators,
+      this.__lengthAsValue,
+      true,
+      this.__optional,
+      this.__refAsValue,
+      this.__conditions
+    );
+
+  }
+
+  /** Looks at value.length instead of value from this point forward. */
+  public get length() {
+
+    return new Validators(
+      this.__validators,
+      true,
+      this.__negate,
+      this.__optional,
+      this.__refAsValue,
+      this.__conditions
+    );
+
+  }
+
+  // Value comparison shorthands
+
+  /** Checks if value is undefined. */
+  public get undefined() { return this.__addValidator(validators.undefined); }
+  /** Checks if value is null. */
+  public get null() { return this.__addValidator(validators.null); }
+  /** Checks if value is NaN. */
+  public get NaN() { return this.__addValidator(validators.NaN); }
+  /** Checks if value is true. */
+  public get true() { return this.__addValidator(validators.true); }
+  /** Checks if value is false. */
+  public get false() { return this.__addValidator(validators.false); }
+  /** Checks is value is truthy. */
+  public get truthy() { return this.__addValidator(validators.truthy); }
+  /** Checks if value is falsey. */
+  public get falsey() { return this.__addValidator(validators.falsey); }
+
+  // Casting validators
+
+  /** Checks if value (casted to number) is greater than target. */
+  public gt(target: number) { return this.__addValidator(validators.gt(target)); }
+  /** Checks if value (casted to number) is greater than target resolved from reference (casted to number). */
+  public gtRef(ref: string) { return this.__addValidator(validators.gtRef(ref)); }
+  /** Checks if value (casted to number) is greater than or equal to target. */
+  public gte(target: number) { return this.__addValidator(validators.gte(target)); }
+  /** Checks if value (casted to number) is greater than or equal to target resolved from reference (casted to number). */
+  public gteRef(ref: string) { return this.__addValidator(validators.gteRef(ref)); }
+  /** Checks if value (casted to number) is less than target. */
+  public lt(target: number) { return this.__addValidator(validators.lt(target)); }
+  /** Checks if value (casted to number) is less than target resolved from reference (casted to number). */
+  public ltRef(ref: string) { return this.__addValidator(validators.ltRef(ref)); }
+  /** Checks if value (casted to number) is less than or equal to target. */
+  public lte(target: number) { return this.__addValidator(validators.lte(target)); }
+  /** Checks if value (casted to number) is less than or equal to target resolved from reference (casted to number). */
+  public lteRef(ref: string) { return this.__addValidator(validators.lteRef(ref)); }
+  /** Checks if value (casted to number) is between the given targets (inclusive). */
+  public between(min: number, max: number) { return this.__addValidator(validators.between(min, max)); }
+  /** Checks if value (casted to number) is between the given targets (exclusive). */
+  public betweenEx(min: number, max: number) { return this.__addValidator(validators.betweenEx(min, max)); }
+  /** Checks if value (casted to number) is a positive number. */
+  public get positive() { return this.__addValidator(validators.positive); }
+  /** Checks if value (casted to number) is a negative number. */
+  public get negative() { return this.__addValidator(validators.negative); }
+  /** Checks if value (casted to number) is zero. */
+  public get zero() { return this.__addValidator(validators.zero); }
+  /** Checks if value (casted to number) is an even number. */
+  public get event() { return this.__addValidator(validators.even); }
+  /** Checks if value (casted to number) is an odd number. */
+  public get odd() { return this.__addValidator(validators.odd); }
+  /** Checks if value (casted to string) is a valid email. */
+  public get email() { return this.__addValidator(validators.email); }
+
+  // Logic gates
+
+  /** ORs all given validators. */
+  public either(
+    validator: ValidatorFunction|AsyncValidatorFunction|Validators,
+    ...additionalValidators: Array<ValidatorFunction|AsyncValidatorFunction|Validators>
+  ) {
+
+    return this.__addValidator(validators.or(...[].concat(
+      validator instanceof Validators ? validator.__exec() : validator,
+      ...additionalValidators.map(validator => validator instanceof Validators ? validator.__exec() : validator)
+    )));
+
+  }
+
+  /** Runs the validators if all passed-in validators pass the validation. */
+  public when(
+    validator: ValidatorFunction|AsyncValidatorFunction|Validators,
+    ...additionalValidators: Array<ValidatorFunction|AsyncValidatorFunction|Validators>
+  ) {
+
+    this.__conditions = this.__conditions.concat(
+      validator instanceof Validators ? validator.__exec() : validator,
+      ...additionalValidators.map(validator => validator instanceof Validators ? validator.__exec() : validator)
+    );
+
+    return this;
+
+  }
+
+  // Other validators
+
+  /** Checks if value is equal to target. */
+  public equal(target: any) { return this.__addValidator(validators.equal(target)); }
+  /** Checks if value is equal to target resolved from reference. */
+  public equalRef(ref: string) { return this.__addValidator(validators.equalRef(ref)); }
+  /** Checks if value is not undefined. */
+  public get exist() { return this.__addValidator(validators.exist); }
+  /** Checks if value matches a regular expression (using value.match). */
+  public match(regex: RegExp) { return this.__addValidator(validators.match(regex)); }
+  /** Checks if value includes target (using value.includes). */
+  public include(target: any) { return this.__addValidator(validators.include(target)); }
+  /** Checks if value includes target resolved from reference (using value.includes). */
+  public includeRef(ref: string) { return this.__addValidator(validators.includeRef(ref)); }
+  /** Checks if value includes all targets (using value.includes). */
+  public includeAll(...targets: any[]) { return this.__addValidator(validators.includeAll(...targets)); }
+  /** Checks if value is included in target (using target.includes). */
+  public in(target: any) { return this.__addValidator(validators.in(target)); }
+  /** Checks if value is included in target resolved from reference (using target.includes). */
+  public inRef(ref: string) { return this.__addValidator(validators.inRef(ref)); }
+  /** Checks if value belongs to enum. */
+  public enum(enumerator: any) { return this.__addValidator(validators.enum(enumerator)); }
+  /** Checks if a valid date can be constructed using the value. */
+  public get date() { return this.__addValidator(validators.date); }
+  /** Checks if value is a valid IANA timezone. */
+  public get timezone() { return this.__addValidator(validators.timezone); }
+  /**
+  * Checks if value is empty.
+  * This validator checks for zero length for strings and arrays, zero value for numbers, and no children for objects.
+  */
+  public get empty() { return this.__addValidator(validators.empty); }
+  /**
+  * Runs the given validator for value's children. Validator can be a validation definition for nested validation.
+  *
+  * If value is an object, a validation definition will check the value's children key-by-key, and a validator function (or executable validators) will check the whole object.
+  * If value is an array, a validation definition will expect an array of objects, checking each item's children key-by-key, while a validator function (or executable validators) will check each item in the array.
+  */
+  public children(
+    validator: ValidationDefinition|ValidatorFunction|AsyncValidatorFunction|Validators,
+    localRefs?: boolean
+  ) {
+
+    return this.__addValidator(validators.children(
+      validator instanceof Validators ? validator.__exec() : validator,
+      localRefs
+    ));
+
+  }
+
+}
+
+/** Aesthetic property. */
+export const should = new Validators();
+/** Aesthetic property. */
+export const be = new Validators();
+/** Aesthetic property. */
+export const and = new Validators();
+/** Aesthetic property. */
+export const have = new Validators();
+/** Aesthetic property. */
+export const a = new Validators();
+/** Aesthetic property. */
+export const an = new Validators();
+/** Aesthetic property. */
+export const been = new Validators();
+/** Aesthetic property. */
+export const has = new Validators();
+/** Aesthetic property. */
+export const of = new Validators();
+/** Aesthetic property. */
+export const to = new Validators();
+/** Aesthetic property. */
+export const belong = new Validators();
+/** Aesthetic property. */
+export const belongs = new Validators();
+/** Aesthetic property. */
+export const it = new Validators();
+/** Aesthetic property. */
+export const its = new Validators();
+/** Aesthetic property. */
+export const must = new Validators();
+/** Aesthetic property. */
+export const these = new Validators();
+/** Aesthetic property. */
+export const that = new Validators();
+/** Aesthetic property. */
+export const as = new Validators();
+/** Aesthetic property. */
+export const are = new Validators();
+/** Aesthetic property. */
+export const is = new Validators();
+/** Aesthetic property. */
+export const which = new Validators();
+/** Aesthetic property. */
+export const where = new Validators();
+/** Aesthetic property. */
+export const does = new Validators();
+/** Optional validation which ignores validation rules when value is undefined. */
+export const could = new Validators([], false, false, true);
+/** Returns validators for the value resolved from a path. */
+export function $(ref: string) { return new Validators([], false, false, false, ref); }
